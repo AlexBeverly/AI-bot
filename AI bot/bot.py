d4c64bc1fa7c7000dae644ca6ce0b1790b2548f9
AlexBeverly <36250419+AlexBeverly@users.noreply.github.com>
2019-07-12 14:06:25 -0700

    add comments

--- AI bot/bot.py
+++ AI bot/bot.py
@@ -5,13 +5,16 @@ import discord
 from chatterbot import ChatBot
 from chatterbot.response_selection import get_random_response
 
+#create bot with name "Bot", equivalent responses are chosen randomly, bot will not continue to learn from conversation
 bot = ChatBot("Bot",
 	response_selection_method = get_random_response,
 	read_only = True)
 
+#insert token for Discord bot here
 TOKEN = ''
 client = discord.Client()
 
+#define response confidence threshold
 THRESHOLD = float("0.34")
 
 @client.event
@@ -23,11 +26,14 @@ async def on_message(message):
         return
     if (message.author.bot):
         return
-    
+
+    #get response from input message
     response = bot.get_response(message.content)
     confidence = float(response.confidence)
+    #return generic response if confidence is too low
     if (confidence < THRESHOLD):
         response = "I didn't understand that."
+    #error caused empty responses, only send response if it exists
     if(len(str(response)) > 1):
         await client.send_message(message.channel, response)
     #print(confidence)
